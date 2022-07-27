0c6ce05476c945ea4434a30535053e52091a297e
Boban Kilvin <btrajcevski10@gmail.com>
2022-07-27 03:34:51 -0400

    Add logic to end game with win/draw

--- script.js
+++ script.js
@@ -19,11 +19,13 @@ const Gameboard = (() => {
 
   const getBoardCell = (index) => board[index];
 
+  const getGameboard = () => board;
+
   const setBoardCell = (index, symbol) => {
     board[index] = symbol;
   };
 
-  return { getBoardCell, setBoardCell };
+  return { getBoardCell, getGameboard, setBoardCell };
 })();
 
 const DisplayController = (() => {
@@ -32,6 +34,56 @@ const DisplayController = (() => {
   let activePlayer = playerOne;
 
   const cells = document.querySelectorAll('button.cell');
+
+  const checkMatchingElements = (array) =>
+    array.every((element) => element === 'X') ||
+    array.every((element) => element === 'O');
+
+  const checkRows = () => {
+    for (let row = 0; row < 3; row += 1) {
+      const rowArray = [];
+
+      for (let cell = row * 3; cell < row * 3 + 3; cell += 1) {
+        rowArray.push(Gameboard.getBoardCell(cell));
+      }
+
+      if (checkMatchingElements(rowArray)) return true;
+    }
+    return false;
+  };
+
+  const checkColumns = () => {
+    for (let column = 0; column < 3; column += 1) {
+      const columnArray = [];
+
+      for (let cell = column; cell <= column + 6; cell += 3) {
+        columnArray.push(Gameboard.getBoardCell(cell));
+      }
+
+      if (checkMatchingElements(columnArray)) return true;
+    }
+    return false;
+  };
+
+  const checkDiagonal = () => {
+    const diagonals = [
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const diagonalGameboardContents = diagonals.map((diagonal) =>
+      diagonal.map((cell) => Gameboard.getBoardCell(cell))
+    );
+
+    for (let i = 0; i < diagonalGameboardContents.length; i += 1) {
+      if (checkMatchingElements(diagonalGameboardContents[i])) return true;
+    }
+
+    return false;
+  };
+
+  const checkWin = () => checkRows() || checkColumns() || checkDiagonal();
+
+  const checkDraw = () => !Gameboard.getGameboard().includes(undefined);
 
   const toggleActivePlayer = () => {
     activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
@@ -58,6 +110,10 @@ const DisplayController = (() => {
   const setMark = (index) => {
     const symbol = activePlayer.get('playerSymbol');
     Gameboard.setBoardCell(index, symbol);
+
+    if (checkWin()) console.log(activePlayer.get('playerSymbol'), 'wins!!!');
+    if (checkDraw()) console.log('Draw!!!');
+
     toggleActivePlayer();
 
     render();
